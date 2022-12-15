8c2f53cdcde8c6133dc280f75d021bbcd634eb60
dr-neptune <mrose4@worcester.edu>
2022-12-15 11:33:41 -0500

    fin

--- day8.rkt
+++ day8.rkt
@@ -1,25 +1,7 @@
 #lang racket
-(require racket math/array racket/pretty (only-in br/list values->list))
-
-(define extrees
-  #<<"
-30373
-25512
-65332
-33549
-35390
-"
-)
+(require racket math/array threading advent-of-code racket/pretty (only-in br/list values->list))
 
 (define trees (fetch-aoc-input (find-session) 2022 8))
-
-;; idea
-;; grab all digits, place in list and convert to array
-(define exarray
-  (~>> extrees
-       string-split
-       (map (compose list->array (λ (c) (map char->integer c)) string->list))
-       array-list->array))
 
 (define tree-heights
   (~>> trees
@@ -27,6 +9,7 @@
        (map (compose list->array (λ (c) (map char->integer c)) string->list))
        array-list->array))
 
+;; split a list into '((before) pos (after))
 (define (split-trio ls pos)
   (let* ([part (values->list (split-at ls pos))]
          [before (first part)]
@@ -34,8 +17,7 @@
          [after (rest (cadr part))])
     (list before val after)))
 
-;; now put it together
-(define (check-cross-section trio)
+(define (check-visibility-cross-section trio)
   (let* ([base-tree (second trio)]
          [side1 (first trio)]
          [side2 (last trio)])
@@ -45,10 +27,10 @@
 (define (visible? r c 2d-array)
   (let ([up-down-split (split-trio (array->list (array-slice-ref 2d-array (list (::) (list c)))) r)]
         [left-right-split (split-trio (array->list (array-slice-ref 2d-array (list (list r) (::)))) c)])
-    (or (check-cross-section up-down-split)
-        (check-cross-section left-right-split))))
+    (or (check-visibility-cross-section up-down-split)
+        (check-visibility-cross-section left-right-split))))
 
-(define visibility
+(define visibility-map
   (let* ([arr-shape (vector->list (array-shape tree-heights))]
          [arr-height (first arr-shape)]
          [arr-width (second arr-shape)])
@@ -57,5 +39,30 @@
         (visible? x y tree-heights))))
 
 ;; pt 1
-(+ (count identity visibility)
-   (* 4 (sub1 (first (vector->list (array-shape tree-heights))))))
+(+ (count identity visibility-map)
+   (* 4 (sub1 (vector-ref (array-shape tree-heights) 0))))
+
+;; pt 2
+(define (count-trees stop-height tree-heights)
+  (cond [(>= (first tree-heights) stop-height) 1]
+        [(equal? (length tree-heights) 1) 1]
+        [else (add1 (count-trees stop-height (rest tree-heights)))]))
+
+(define (get-tree-score trio)
+  (apply * (map ((curry count-trees) (second trio))
+                (list (reverse (first trio)) (last trio)))))
+
+(define (get-tree-visibilities r c 2d-array)
+  (let ([up-down-split (split-trio (array->list (array-slice-ref 2d-array (list (::) (list c)))) r)]
+        [left-right-split (split-trio (array->list (array-slice-ref 2d-array (list (list r) (::)))) c)])
+    (* (get-tree-score up-down-split)
+       (get-tree-score left-right-split))))
+
+;; now we want to apply it to the inner square
+(define tree-visibility-scores
+  (let ([arr-shape (vector->list (array-shape tree-heights))])
+    (for*/list ([x (in-range 1 (sub1 (first arr-shape)))]
+                [y (in-range 1 (sub1 (second arr-shape)))])
+      (get-tree-visibilities x y tree-heights))))
+
+(apply max tree-visibility-scores)
