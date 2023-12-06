96a4e2c6b96525d506333341b2e48bfb05ca3421
dr-neptune <mrose4@worcester.edu>
2023-12-06 13:16:36 -0500

    palm's last album :'(

--- 2023/day-6.rkt
+++ 2023/day-6.rkt
@@ -7,7 +7,11 @@
          (only-in srfi/1 unfold-right)
          (only-in srfi/26 cut))
 
+;; data shaping
 (define boat-races (string-split (fetch-aoc-input (find-session) 2023 6) "\n"))
+
+(define (string-eval-parser str-in parser)
+  (parse-result! (parse-string parser str-in)))
 
 (define boat-races/p
   (let ([space+ (many/p space/p)])
@@ -19,7 +23,7 @@
       (pure boat-ints))))
 
 (define time-dists
-  (let ([parsed (map (compose parse-result! (curry parse-string boat-races/p)) boat-races)])
+  (let ([parsed (map (curryr string-eval-parser boat-races/p) boat-races)])
     (map cons (first parsed) (second parsed))))
 
 (define (digit-list->int ls)
@@ -31,16 +35,21 @@
 ;; part 1
 (define (winning-ways time-dists)
   (let* ([move-fn (λ (v t) (* v (- t v)))]
-         [check-fn (λ (t dist)
-                     (stream->list
-                      (stream-filter (curry < dist)
-                                     (stream-map (curryr move-fn t) (in-range 0 t)))))])
+         [check-fn
+          (λ (t dist)
+            ((compose stream->list
+                      (curry stream-filter (curry < dist))
+                      (curry stream-map (curryr move-fn t)))
+             (in-range t)))])
     (apply * (map (λ (td) (length (check-fn (car td) (cdr td)))) time-dists))))
+
+(winning-ways time-dists)
 
 ;; part 2
 (define concat-time-dists
-  (let ([concat-digit-list (λ (digit-list) (digit-list->int (flatten (map int->digit-list digit-list))))])
-    (cons (concat-digit-list (map car time-dists))
-          (concat-digit-list (map cdr time-dists)))))
+  (let* ([digit-concat (compose digit-list->int flatten (curry map int->digit-list))]
+         [concat-digit-list (λ (digit-list) (digit-concat digit-list))]
+         [get-concat-num (λ (f) (concat-digit-list (map f time-dists)))])
+    (list (cons (get-concat-num car) (get-concat-num cdr)))))
 
-(winning-ways (list concat-time-dists))
+(winning-ways concat-time-dists)
