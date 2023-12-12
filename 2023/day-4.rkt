4b6173f1424179a08840a61423bea69344d5d16d
dr-neptune <mrosedatasci@gmail.com>
2023-12-12 17:33:17 -0500

    do svidanya, caroline

--- 2023/day-4.rkt
+++ 2023/day-4.rkt
@@ -1,18 +1,6 @@
 #lang racket
 (require racket advent-of-code data/monad data/applicative megaparsack megaparsack/text)
 
-(define scratch-cards (string-split (fetch-aoc-input (find-session) 2023 4) "\n"))
-
-(define scratch-cards
-  (string-split
-   "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
-   "\n"))
-
 (define card/p
   (let ([space+ (many/p space/p)])
     (do (string/p "Card")
@@ -25,10 +13,12 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
       [your-numbers <- (many/p integer/p #:sep (many/p space/p))]
       (pure (cons n (list (first card-numbers) your-numbers))))))
 
+(define scratch-cards (string-split (fetch-aoc-input (find-session) 2023 4) "\n"))
 (define parsed-cards (map (compose parse-result! (curry parse-string card/p)) scratch-cards))
+
+;; part 1
 (define calc-points (λ (winners) (ceiling (expt 2 (sub1 (length winners))))))
 (define original-scratch (map (λ (card) (set-intersect (cadr card) (last card))) parsed-cards))
-
 (apply + (map calc-points (filter (compose not empty?) original-scratch)))
 
 ;; part 2
