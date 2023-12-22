6de263cc978bcf3aadd99710018644c4a95b8c89
dr-neptune <mrosedatasci@gmail.com>
2023-12-21 22:52:29 -0500

    no, I get it

--- 2023/day-7.rkt
+++ 2023/day-7.rkt
@@ -16,8 +16,7 @@ QQQJA 483" "\n"))
 (define (split-where ls val)
   (let ([split-point (index-of ls val)])
     (if split-point
-        (list (take ls split-point)
-              (drop ls split-point))
+        (list (take ls split-point) (drop ls split-point))
         (list ls '()))))
 
 (define hands
@@ -36,14 +35,11 @@ QQQJA 483" "\n"))
            [lesser lesser]
            [greater greater]))))))
 
-(define card-order (comparator-of-constants #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\T #\J #\Q #\K #\A))
-(define card-order-rank<=> (card-comp-gen card-order))
-
-(define hand-rank<=>
+(define (hand-rank-gen group-fn)
   (make-comparator
    (λ (a b)
      (let ([hand-mapping
-            (λ (hand) (match (sort (map length (group-by identity hand)) <)
+            (λ (hand) (match (sort (map length (group-fn hand)) <)
                         ['(5) 1] ;; 5 of a kind
                         ['(1 4) 2] ;; 4 of a kind
                         ['(2 3) 3] ;; full house
@@ -56,18 +52,23 @@ QQQJA 483" "\n"))
              [(= (hand-mapping a) (hand-mapping b)) equivalent]
              [(> (hand-mapping a) (hand-mapping b)) greater])))))
 
-(define sorted-hands
+
+(define (sort-hands hands hand-rank<=> card-order-rank<=>)
   (transduce (hash-keys hands)
              (sorting (comparator-chain hand-rank<=> (comparator-reverse card-order-rank<=>)))
              #:into into-list))
 
-(apply + (map (λ (a b) (* (hash-ref hands a) b))
-              sorted-hands (stream->list (in-inclusive-range (length sorted-hands) 1 -1))))
-
-;; part 2
-(define card-order (comparator-of-constants #\J #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\T #\Q #\K #\A))
+;; step 1
+(define hand-rank<=> (hand-rank-gen (curry group-by identity)))
+(define card-order (comparator-of-constants #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\T #\J #\Q #\K #\A))
 (define card-order-rank<=> (card-comp-gen card-order))
 
+
+(let ([sorted-hands (sort-hands hands hand-rank<=> card-order-rank<=>)])
+  (apply + (map (λ (a b) (* (hash-ref hands a) b))
+                sorted-hands (stream->list (in-inclusive-range (length sorted-hands) 1 -1)))))
+
+;; part 2
 (define (joker-identity-group-by ls)
   (let ([non-jokers (transduce
                      (remove* (list #\J) ls)
@@ -79,28 +80,11 @@ QQQJA 483" "\n"))
           [(empty? non-jokers) (list (make-list joker-count #\J) '())]
           [else (list-update (gb non-jokers) 0 (compose flatten (curry cons (make-list joker-count #\J))))])))
 
-(define hand-rank<=>
-  (make-comparator
-   (λ (a b)
-     (let ([hand-mapping
-            (λ (hand) (match (sort (map length (joker-identity-group-by hand)) <)
-                        ['(5) 1] ;; 5 of a kind
-                        ['(1 4) 2] ;; 4 of a kind
-                        ['(2 3) 3] ;; full house
-                        ['(1 1 3) 4] ;; 3 of a kind
-                        ['(1 2 2) 5] ;; 2 pair
-                        ['(1 1 1 2) 6]  ;; one pair
-                        ['(1 1 1 1 1) 7] ;; distinct
-                        [_ 8]))])
-       (cond [(< (hand-mapping a) (hand-mapping b)) lesser]
-             [(= (hand-mapping a) (hand-mapping b)) equivalent]
-             [(> (hand-mapping a) (hand-mapping b)) greater])))))
-
-(define sorted-hands
-  (transduce (hash-keys hands)
-             (sorting (comparator-chain hand-rank<=> (comparator-reverse card-order-rank<=>)))
-             #:into into-list))
+(define hand-rank<=> (hand-rank-gen joker-identity-group-by))
+(define card-order (comparator-of-constants #\J #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\T #\Q #\K #\A))
+(define card-order-rank<=> (card-comp-gen card-order))
 
 ;; not working. example works, input does not
-(apply + (map (λ (a b) (* (hash-ref hands a) b))
-              sorted-hands (stream->list (in-inclusive-range (length sorted-hands) 1 -1))))
+(let ([sorted-hands (sort-hands hands hand-rank<=> card-order-rank<=>)])
+  (apply + (map (λ (a b) (* (hash-ref hands a) b))
+                sorted-hands (stream->list (in-inclusive-range (length sorted-hands) 1 -1)))))
