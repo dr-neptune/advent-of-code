d8bafa1bf977803c0218415f98a5886c79f240ca
dr-neptune <mrosedatasci@gmail.com>
2024-08-30 07:34:51 -0400

    yep

--- 2020/day-3.rkt
+++ 2020/day-3.rkt
@@ -2,41 +2,13 @@
 (require racket threading advent-of-code)
 
 (define tree-grid (~> (fetch-aoc-input (find-session) 2020 3) (string-split "\n")))
-(define tree-grid (string-split tree-grid "\n"))
-
-
-;; (define exstr
-;;   "..##.........##.........##.........##.........##.........##.......
-;; #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
-;; .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
-;; ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
-;; .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
-;; ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
-;; .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
-;; .#........#.#........#.#........#.#........#.#........#.#........#
-;; #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
-;; #...##....##...##....##...##....##...##....##...##....##...##....#
-;; .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#")
-
-;; (define tree-grid (string-split exstr "\n"))
-
-;;  The tree pattern repeats itself infinitely to the right!
-(for/fold ([points '()]
-           [curr-point 3]
-           #:result (count (curry equal? #\#) points))
-          ([tree-line (rest tree-grid)]
-           #:do [(define trees (string->list tree-line))])
-  (values (cons (list-ref trees curr-point) points)
-          (remainder (+ curr-point 3) (length trees))))
-
-;; part 2
 
 (define (get-slope-count right down)
   (for/fold ([points '()]
              [curr-point right]
              [idx 0]
              #:result (count (curry equal? #\#) points))
-            ([tree-line (rest tree-grid)]
+            ([tree-line (drop tree-grid down)]
              #:do [(define trees (string->list tree-line))])
     (if (zero? (remainder idx down))
         (values (cons (list-ref trees curr-point) points)
@@ -44,9 +16,19 @@
                 (add1 idx))
         (values points curr-point (add1 idx)))))
 
+;; part 1
+(get-slope-count 3 1)
 
+;; part 2
 (* (get-slope-count 1 1)
    (get-slope-count 3 1)
    (get-slope-count 5 1)
    (get-slope-count 7 1)
    (get-slope-count 1 2))
+
+
+(let ([skip-iter 3])
+  (for/list ([idx (in-naturals)]
+             [ls-ele '(1 2 3 4 5 6)])
+    (when (zero? (remainder idx skip-iter))
+      ls-ele)))
