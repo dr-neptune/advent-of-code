f1b210a2e4839ed564a6d94420dce2cc2d33d218
dr-neptune <mrosedatasci@gmail.com>
2024-09-17 17:19:52 -0400

    working on it

--- 2020/day-16.rkt
+++ 2020/day-16.rkt
@@ -28,31 +28,31 @@
 
 (define ticket-info
   (~>>
-;; "class: 1-3 or 5-7
-;; row: 6-11 or 33-44
-;; seat: 13-40 or 45-50
+   ;; "class: 1-3 or 5-7
+   ;; row: 6-11 or 33-44
+   ;; seat: 13-40 or 45-50
 
-;; your ticket:
-;; 7,1,14
+   ;; your ticket:
+   ;; 7,1,14
 
-;; nearby tickets:
-;; 7,3,47
-;; 40,4,50
-;; 55,2,20
-;; 38,6,12"
-"class: 0-1 or 4-19
-row: 0-5 or 8-19
-seat: 0-13 or 16-19
+   ;; nearby tickets:
+   ;; 7,3,47
+   ;; 40,4,50
+   ;; 55,2,20
+   ;; 38,6,12"
+   ;; "class: 0-1 or 4-19
+   ;; row: 0-5 or 8-19
+   ;; seat: 0-13 or 16-19
 
-your ticket:
-11,12,13
+   ;; your ticket:
+   ;; 11,12,13
 
-nearby tickets:
-3,9,18
-15,1,5
-5,14,9
-"
-;;   (fetch-aoc-input (find-session) 2020 16)
+   ;; nearby tickets:
+   ;; 3,9,18
+   ;; 15,1,5
+   ;; 5,14,9
+   ;; "
+   (fetch-aoc-input (find-session) 2020 16)
    (string-split _ "\n\n")
    (map (λ~> (string-split "\n")))
    ((λ (tri)
@@ -99,35 +99,27 @@ let's start by discarding the invalid numbers
 (define (list-contains-any? base-list membership-list)
   (ormap (lambda (x) (not (not (member x membership-list)))) base-list))
 
+(define (ranges-contains? ranges v) (ormap (λ~> (range-contains? v)) ranges))
+
 (define valid-tickets
   (filter
    (compose not (curryr list-contains-any? invalid-numbers))
    (third ticket-info)))
 
+(define (range-union-contains? ranges v)
+  (ormap (λ~> (range-contains? v)) ranges))
 
-#|
-
-idea
-which permutation of the first value fits the ranges?
-of the second?
-
-can we skimp out and only do a couple?
-
-|#
-
-(let* ([field-ranges (flatten (map field-ranges (first ticket-info)))]
-       [check-match
-        (λ (permutation)
-          (andmap identity (map (λ (range val) (range-contains? range val)) field-ranges permutation)))])
-  (for/list ([ticket valid-tickets])
-    (for/list ([perm (in-permutations ticket)]
-               ;; #:when (check-match perm)
-               )
-      (list
-       perm
-       (map (λ (range val) (range-contains? range val)) field-ranges perm))
-      )))
-
-;; need to get a position
-;; maybe we should permute the ranges instead?
-;; maybe it wouldn't matter, think about it though
+(pretty-display
+ (apply set-intersect
+  (let* ([fields (first ticket-info)])
+    (for/list ([ticket valid-tickets])
+      (for/fold ([solns (mutable-set)])
+                ([perm (in-permutations fields)]
+                 #:when (andmap (λ (ru t) (range-union-contains? ru t)) (map field-ranges perm) ticket)
+                 #:break (equal? (set-count solns) 1))
+        (let ([res (map field-name perm)])
+          (if (set-empty? solns)
+              res
+              (begin
+                ;; (displayln solns)
+                (set-intersect res solns)))))))))
