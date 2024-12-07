143bf926afe122caa5ba9c844099ef8184a7ccd0
dr-neptune <mrose4@worcester.edu>
2024-12-07 14:52:52 -0500

    day yeven

---
+++ 2024/day-7.rkt
@@ -0,0 +1,44 @@
+#lang racket
+(require racket threading advent-of-code memo)
+
+(define equations
+  (map (λ (line)
+         (match (string-split line ": ")
+           [(list sum-str summands-str)
+            (list (string->number sum-str)
+                  (map string->number (string-split summands-str " ")))]))
+       (string-split (fetch-aoc-input (find-session) 2024 7 #:cache #t) "\n")))
+
+(define (interleave ls1 ls2)
+  (if (null? ls1) ls2 (cons (car ls1) (interleave ls2 (cdr ls1)))))
+
+(define/memoize (n-cart-prod elements size)
+  (apply cartesian-product (make-list size elements)))
+
+(define/memoize (r->l-eval ls)
+  (let loop ([stack '()] [ls ls])
+    (if (empty? ls) (first stack)
+        (match-let ([(list* fele rele) ls])
+          (if (number? fele)
+              (loop (cons fele stack) rele)
+              (loop (list (fele (first stack) (car rele))) (rest rele)))))))
+
+(define (calculate-calibrations equations operators)
+  (let ([get-op-match (λ (sum summands)
+           (for/first ([operator-combination (n-cart-prod operators (sub1 (length summands)))]
+                       #:do [(define eqn-eval (r->l-eval (interleave summands operator-combination)))]
+                       #:when (equal? eqn-eval sum))
+             eqn-eval))])
+    (for/sum ([eq equations]
+              #:do [(match-define (list sum summands) eq)
+                    (define op-match (get-op-match sum summands))]
+              #:when (not (false? op-match)))
+      op-match)))
+
+;; pt 1
+(calculate-calibrations equations (list + *))
+
+;; pt 2
+(define (|| a b) (~>> (list a b) (map number->string) (apply string-append) string->number))
+
+(calculate-calibrations equations (list || + *))
