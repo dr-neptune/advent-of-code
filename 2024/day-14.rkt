97cfc2db2e076c64a6dae53d87d01a3362c4119b
dr-neptune <mrose4@worcester.edu>
2024-12-14 21:30:16 -0500

    let it be a mess

---
+++ 2024/day-14.rkt
@@ -0,0 +1,146 @@
+#lang racket
+(require racket threading math/statistics "../utils.rkt")
+
+(define robots
+  (~> (get-aoc 2024 14) (string-split "\n")
+      (map (λ~>> (regexp-match* #px"[+-]?\\d+") (map string->number)) _)))
+
+;; (match-define (list grid-width grid-height) (list 11 7))
+(match-define (list grid-width grid-height) (list 101 103))
+
+(pretty-print-columns (* 10 grid-width))
+
+(define (calculate-loc s x y vx vy)
+  (let rec ([s s] [x x] [y y])
+    (if (zero? s) (list x y)
+        (let ([new-x (+ x vx)] [new-y (+ y vy)]
+              [circle (λ (v dist) (if (negative? v) (+ dist v) (remainder v dist)))])
+          (rec (sub1 s) (circle new-x grid-width) (circle new-y grid-height))))))
+
+(define (quadrant ele)
+  (match-let* ([(list ele-x ele-y) ele]
+               [v-/ (curryr quotient 2)]
+               [x-/ (v-/ grid-width)]
+               [y-/ (v-/ grid-height)])
+    (cond [(and (< ele-x x-/) (< ele-y y-/)) 1]
+          [(and (< x-/ ele-x) (< ele-y y-/)) 2]
+          [(and (< ele-x x-/) (< y-/ ele-y)) 3]
+          [(and (< x-/ ele-x) (< y-/ ele-y)) 4]
+          [else 0])))
+
+;; part 1
+(~>> robots
+     (map (λ~>> (apply (curry calculate-loc 100)) quadrant))
+     (group-by identity)
+     (filter (λ~> car zero? not))
+     (map length)
+     (apply *))
+
+;; part 2
+;; repl abuse?
+
+(define green "\x1b[32m")
+(define red "\x1b[31m")
+(define reset "\x1b[0m")
+
+(define (make-grid coords s [max-x grid-width] [max-y grid-height])
+  (define grid
+    (for/vector ([row (in-range (add1 max-y))])
+      (make-vector (add1 max-x) ".")))
+
+  ;; Place a green "^" at each coordinate
+  (for ([pt coords])
+    (define x (first pt))
+    (define y (second pt))
+    (vector-set! (vector-ref grid y) x (string-append green "^" reset)))
+
+  (define divider (make-vector (+ 4 grid-width) (string-append red "-" reset)))
+
+  (displayln divider)
+  (displayln (string-append green "\t\t\t\tIteration: " (number->string s) reset))
+  (displayln "")
+  (pretty-display grid)
+  (displayln "")
+  (displayln divider))
+
+(define (median-of-pairwise-distances points)
+  (define (pairwise-distances points)
+    (for*/list ([i (in-range (length points))]
+                [j (in-range (add1 i) (length points))])
+      (let* ([p1 (list-ref points i)]
+             [x1 (first p1)]
+             [y1 (second p1)]
+             [p2 (list-ref points j)]
+             [x2 (first p2)]
+             [y2 (second p2)]
+             [dx (- x1 x2)]
+             [dy (- y1 y2)])
+        (sqrt (+ (* dx dx) (* dy dy))))))
+  (median < (pairwise-distances points)))
+
+(define warmup-iters 50)
+(define stddev-below-mean 5)
+
+(for/fold ([robots robots] [stat empty-statistics] [min-dist-mean +Inf.0])
+          ([s (in-range 10000)])
+  (let* ([new-robos
+          (map (λ (robot) (append (apply calculate-loc (cons 1 robot))
+                                  (take-right robot 2)))
+               robots)]
+         [robo-points (map (curryr take 2) new-robos)]
+         [pairwise-dist-median (median-of-pairwise-distances robo-points)]
+         [stat (update-statistics stat pairwise-dist-median)]
+         [curr-mean (statistics-mean stat)] [curr-stddev (statistics-stddev stat #:bias #t)]
+         [new-threshold (- curr-mean (* stddev-below-mean curr-stddev))])
+    (when (and (< pairwise-dist-median new-threshold)
+               (< warmup-iters s)
+               (< pairwise-dist-median min-dist-mean))
+      (displayln (format "iteration: ~a dist median: ~a mean: ~a stddev: ~a threshold: ~a min: ~a"
+                         s pairwise-dist-median curr-mean curr-stddev new-threshold min-dist-mean))
+      (make-grid robo-points s))
+
+    (when (zero? (modulo s 1000))
+      (displayln (format "On Iteration ~a. Current mean: ~a" s (statistics-mean stat))))
+
+    (values new-robos stat (min pairwise-dist-median min-dist-mean))))
+
+;; idea
+;; if this min thing works
+;; we can find it by just successively finding the minimal dist plots
+;; then we don't even need all the statistics
+
+(for/fold ([robots robots] [min-dist-median +Inf.0])
+          ([s (in-range 10000)])
+  (let* ([new-robos
+          (map (λ (robot) (append (apply calculate-loc (cons 1 robot))
+                                  (take-right robot 2)))
+               robots)]
+         [robo-points (map (curryr take 2) new-robos)]
+         [pairwise-dist-median (median-of-pairwise-distances robo-points)])
+    (when (< pairwise-dist-median min-dist-median)
+      (displayln (format "iteration: ~a dist median: ~a" s pairwise-dist-median))
+      (make-grid robo-points s))
+
+    (when (zero? (modulo s 1000))
+      (displayln (format "On Iteration ~a. Current min: ~a" s min-dist-median)))
+
+    (values new-robos (min pairwise-dist-median min-dist-median))))
+
+;; idea 3, randomly sample to speed it up
+
+(for/fold ([robots robots] [min-dist-median +Inf.0])
+          ([s (in-range 10000)])
+  (let* ([new-robos
+          (map (λ (robot) (append (apply calculate-loc (cons 1 robot))
+                                  (take-right robot 2)))
+               robots)]
+         [robo-points (map (curryr take 2) new-robos)]
+         [pairwise-dist-median (median-of-pairwise-distances (take (shuffle robo-points) 100))])
+    (when (< pairwise-dist-median min-dist-median)
+      (displayln (format "iteration: ~a dist min: ~a" s pairwise-dist-median))
+      (make-grid robo-points s))
+
+    (when (zero? (modulo s 1000))
+      (displayln (format "On Iteration ~a. Current min: ~a" s min-dist-median)))
+
+    (values new-robos (min pairwise-dist-median min-dist-median))))
