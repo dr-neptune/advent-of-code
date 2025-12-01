6b839a18c4f21533caa63a021025ea3c169e8d87
dr-neptune <mrosedatasci@gmail.com>
2025-12-01 18:59:21 -0500

    Clean up 2025 day 1 parsing and zero counting

--- 2025/day-1.rkt
+++ 2025/day-1.rkt
@@ -1,26 +1,14 @@
 #lang racket
-(require racket advent-of-code threading
-         (only-in br/list values->list))
+(require racket advent-of-code)
 
 (define input (fetch-aoc-input (find-session) 2025 1))
-
-(define input "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82")
 
 (define rotation-doc
   (for/list ([dial (in-list (regexp-match* #px"([LR])(\\d+)" input #:match-select values))])
     (match-let* ([(list _ dir amt) dial] [amt (string->number amt)])
       (if (string=? dir "L") (- amt) amt))))
 
-;; part 1
+;; part 1: count landings on zero
 (for/fold ([num 50] [zeros 0])
           ([turn rotation-doc])
   (let ([curr (modulo (+ num turn) 100)])
@@ -28,12 +16,11 @@ L82")
         (values curr (add1 zeros))
         (values curr zeros))))
 
-;; part 2
+;; part 2: also count passes over zero mid-rotation
 (define (count-zeros num turn)
-  (let* ([rng (if (negative? turn) (in-range 0 turn -1) (in-range turn))])
-    (for/sum ([offset rng]
-              #:do [(define curr (modulo (+ num offset) 100))]
-              #:when (zero? curr))
+  (let ([step (if (negative? turn) -1 1)])
+    (for/sum ([offset (in-inclusive-range step turn step)]
+              #:when (zero? (modulo (+ num offset) 100)))
       1)))
 
 (for/fold ([num 50] [zeros 0])
